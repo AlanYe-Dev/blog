a41f7fa77b3973cb41cd6decafaaa0696ed46d5f
Alan Ye <git@wrye.dev>
2024-07-15 22:49:29 +0800

    config: edit site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://wrye.dev/",
   base: "/",
   trailingSlash: "always",
   integrations: [
