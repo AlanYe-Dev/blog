30f71bb39be291a5927b63e9554f9f38001728a3
Alan Ye <git@wrye.dev>
2024-08-11 17:34:22 +0800

    fix: correct link to Telegram

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const profileConfig: ProfileConfig = {
       icon: 'fa6-brands:telegram',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://telegram.org/WrOffi',
+      url: 'https://t.me/WrOffi',
     },
     {
       name: 'Twitter',
